f5c558778e08269054919210e159a343c1b308c0
Gabriel Fonseca <gabriel.fonseca@live.com>
2023-05-31 19:18:24 -0300

    fix login

--- src/services/login.service.ts
+++ src/services/login.service.ts
@@ -1,7 +1,6 @@
 import jwt from 'jsonwebtoken';
 import userModel from '../models/user.model';
 import { config, secret } from '../middlewares/jwtConfig';
-// import { ReturnReq, ReturnReqLogin } from '../interfaces';
 
 async function login(username: string, password: string) {
   const user = await userModel.getByUsername(username);
